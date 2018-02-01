a7cc7f838ca1883ee9fc4afa63be7f5ffc8d470f
Marcus Ball <marcus@marcusball.me>
2018-01-31 23:09:10 -0500

    Add std::io::Error as a supported error type

--- src/error.rs
+++ src/error.rs
@@ -6,6 +6,7 @@ pub enum Error {
     SanedError(Status),
     BadNetworkDataError(String),
     FromUtf8Error(::std::string::FromUtf8Error),
+    IOError(::std::io::Error),
 }
 
 impl From<Status> for Error {
@@ -17,5 +18,11 @@ impl From<Status> for Error {
 impl From<::std::string::FromUtf8Error> for Error {
     fn from(error: ::std::string::FromUtf8Error) -> Error {
         Error::FromUtf8Error(error)
+    }
+}
+
+impl From<::std::io::Error> for Error {
+    fn from(error: ::std::io::Error) -> Error {
+        Error::IOError(error)
     }
 }
